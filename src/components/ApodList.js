0831351cd958ca6f466131a3e4236c2513b6169d
VinoMurugan <137902991+VinoMurugan@users.noreply.github.com>
2023-10-11 20:16:50 -0400

    Update ApodList.js

--- src/components/ApodList.js
+++ src/components/ApodList.js
@@ -1,29 +1,4 @@
-// // src/components/ApodList.js
-// import React from 'react';
 
-// const ApodList = ({ apodEntries }) => {
-//   if (!apodEntries) {
-//     return <p>Loading...</p>; // or any other appropriate loading state
-//   }
-
-//   return (
-//     <div>
-//       <h1>Astronomy Picture of the Day (APOD)</h1>
-//       <ul>
-//         {apodEntries.map((entry, index) => (
-//           <li key={index}>
-//             <h2>{entry.title}</h2>
-//             <p>Date: {entry.date}</p>
-//             <p>{entry.description}</p>
-//             <img src={entry.imageUrl} alt={entry.title} />
-//           </li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default ApodList;
 
 import React from 'react';
 
